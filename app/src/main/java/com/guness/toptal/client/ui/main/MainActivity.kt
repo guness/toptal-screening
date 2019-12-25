e5dcc5dab196c678819abfb1a6f2967a51cf58fc
Sinan Gunes <toptal@sinangunes.com>
2019-12-25 13:20:52 +0300

    Hiding, showing fab on search open/close

--- app/src/main/java/com/guness/toptal/client/ui/main/MainActivity.kt
+++ app/src/main/java/com/guness/toptal/client/ui/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.guness.toptal.client.ui.main
 
 import android.view.Menu
-import android.view.MenuItem
+import android.widget.SearchView
 import com.guness.toptal.client.R
 import com.guness.toptal.client.core.BaseActivity
 import com.guness.toptal.client.ui.entry.NewEntryActivity
@@ -9,6 +9,7 @@ import com.guness.toptal.client.utils.extensions.startActivity
 import com.guness.toptal.client.utils.listView.ListAdapter
 import com.guness.toptal.client.utils.listView.SingleTypeItemLayout
 import com.jakewharton.rxbinding2.view.clicks
+import com.jakewharton.rxbinding2.widget.queryTextChanges
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.plusAssign
 import kotlinx.android.synthetic.main.activity_main.*
@@ -34,21 +35,25 @@ class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class.java, R.la
         disposables += viewModel.entries
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(adapter::update)
+
+        disposables += viewModel.filter.skip(1).subscribe { toolbar.performShow() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.bottom_app_bar_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            //R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val searchView = menu.findItem(R.id.bottom_app_bar_menu_search).actionView as SearchView
+        searchView.maxWidth = Integer.MAX_VALUE
+        searchView.setOnSearchClickListener {
+            fab.hide()
+            toolbar.navigationIcon = null
         }
+        searchView.setOnCloseListener {
+            fab.show()
+            toolbar.setNavigationIcon(R.drawable.ic_menu_on_surface_24dp)
+            false
+        }
+        searchView.queryTextChanges().subscribe(viewModel.filter)
+        return true
     }
 }
--- app/src/main/java/com/guness/toptal/client/ui/main/MainViewModel.kt
+++ app/src/main/java/com/guness/toptal/client/ui/main/MainViewModel.kt
@@ -8,6 +8,9 @@ import com.guness.toptal.client.data.repositories.ProfileRepository
 import com.guness.toptal.client.utils.extensions.mapList
 import com.guness.toptal.client.utils.listView.DefaultAdapterModel
 import com.guness.toptal.client.utils.listView.SingleTypeItemLayout
+import io.reactivex.BackpressureStrategy
+import io.reactivex.rxkotlin.Flowables
+import io.reactivex.subjects.BehaviorSubject
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
@@ -16,7 +19,18 @@ class MainViewModel @Inject constructor(
     private val profileRepository: ProfileRepository
 ) : BaseViewModel() {
 
-    val entries = entryRepository.entries()
+    val filter = BehaviorSubject.createDefault<CharSequence>("")
+
+    val entries = Flowables.combineLatest(entryRepository.entries(), filter.toFlowable(BackpressureStrategy.LATEST))
+        .map { pair ->
+            val query = pair.second.trim()
+            val list = pair.first
+            if (query.isEmpty()) {
+                list
+            } else {
+                list.filter { it.name.contains(query, true) || it.city.contains(query, true) }
+            }
+        }
         .mapList { SingleTypeItemLayout(R.layout.item_row_time_entry, TimeEntryItemViewModel(it)) }
         .map { DefaultAdapterModel(it.toTypedArray()) }
 
